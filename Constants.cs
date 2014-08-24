b46563568f321c3eb507d0fa70c78e7484382573
Jurgen <jurgen.welschen@gmail.com>
2014-08-25 00:01:27 +0200

    some weird stuff going on here!

--- Constants.cs
+++ Constants.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MVCTemplate.Library
+namespace Base.Library
 {
     public class Constants
     {
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVCTemplate.Library")]
+[assembly: AssemblyTitle("Base.Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MVCTemplate.Library")]
+[assembly: AssemblyProduct("Base.Library")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
